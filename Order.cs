1b552a52249bc5de224297833de2738f27f28331
Hanna Baranowska <s193531@student.pg.edu.pl>
2025-03-14 19:01:57 +0100

    General app flow

---
+++ Order.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+enum OrderStatus
+{
+    Nowe,
+    WMagazynie,
+    Wysylka,
+    Zwrocone,
+    Blad,
+    zamknięte
+}
+class Order
+{
+    private int id { get; set; }
+    private int amount { get; set; }
+    private string nazwa { get; set; }
+    private string klientType { get; set; }
+    private string adres { get; set; }
+    private string paymentMethod { get; set; }
+
+    private static List<Order> orders = new List<Order>();
+
+    private static int nextOrder = 0;
+    private OrderStatus Status { get; set; }
+
+    public Order(string nazwa, int amount, string klientType, string adres, string paymentMethod)
+    { 
+        this.amount = amount;
+        this.nazwa = nazwa;
+        this.klientType = klientType;
+        this.paymentMethod = paymentMethod;
+        this.adres = adres;
+        this.id = nextOrder++;
+    }
+    public Order(int id) { }
+
+    public void MakeOrder()
+    {
+        orders.Add(this);
+        orders[id].Status = OrderStatus.Nowe;
+
+        Console.WriteLine("Stworzono zamówienie:");
+        Console.WriteLine($"Nazwa: {this.nazwa}");
+        Console.WriteLine($"Ilość: {this.amount}");
+        Console.WriteLine($"Klient: {this.klientType}");
+        Console.WriteLine($"Adres: {this.adres}");
+        Console.WriteLine($"Metoda płatności: {this.paymentMethod}");
+
+    }
+    public static void MoveOrderToMagazine(int id)
+    {
+        Console.SetCursorPosition(2, 2);
+        orders[id].Status = OrderStatus.WMagazynie;
+    }
+    public static void MoveOrderToShipment(int id)
+    {
+        Console.SetCursorPosition(2, 2);
+        orders[id].Status = OrderStatus.Wysylka;
+    }
+    public static void SeeOrders()
+    {
+        foreach(Order order in orders)
+        {
+            Console.WriteLine($"ID: {order.id}, Nazwa: {order.nazwa}, Ilość: {order.amount}, Status: {order.Status}, Sposób płatności: {order.paymentMethod}");
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Specialized;
+using System.Reflection.Metadata;
+
+class Program
+{
+    static void displayWindow()
+    {
+        Console.Clear();
+        for (int j = 0; j < 42; j++)
+        {
+            Console.Write("*");
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(" ");
+            Console.Write("*");
+            for (int j = 0; j < 40; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("*");
+            Console.WriteLine(" ");
+        }
+        for (int j = 0; j < 42; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+    
+    static void exit()
+    {
+        Console.SetCursorPosition(2, 2);
+
+    }
+    static void menu()
+    {
+        Console.SetCursorPosition(2, 2);
+        Console.WriteLine("Menu:");
+        Console.WriteLine("1. Utwórz przykładowe zamówienie");
+        Console.WriteLine("2. Przekaż zamówienie do magazynu");
+        Console.WriteLine("3. Przekazanie zamówienia do wysyłki");
+        Console.WriteLine("4. wyświetl liste zamówień");
+        Console.WriteLine("4. Wyjście");
+        Console.WriteLine("Wybierz numer:");
+        string userInput = Console.ReadLine();
+
+        int menuNumber;
+
+        if (int.TryParse(userInput, out menuNumber))
+        {
+            if (menuNumber == 1)
+            {
+                Order order = new Order("Laptop", 9,"ss", "xx","xx");
+                order.MakeOrder();
+                
+            }else if(menuNumber == 2)
+            {
+                Console.WriteLine("Podaj ID zamówienia do przeniesienia do magazynu:");
+                int id = int.Parse(Console.ReadLine());
+                Order.MoveOrderToMagazine(id);
+            }
+            else if (menuNumber == 3)
+            {
+                Console.WriteLine("Podaj ID zamówienia do przeniesienia do wysyłki:");
+                int id = int.Parse(Console.ReadLine());
+                Order.MoveOrderToShipment(id);
+            }
+            else if (menuNumber == 4)
+            {
+                Order.SeeOrders();
+            }
+            Console.WriteLine("Nacisnij dowolny klawisz, aby wrócić do menu");
+            Console.ReadKey();
+        }
+    }
+    static void Main()
+    {
+        while (true)
+        {
+            displayWindow();
+            menu();
+        }
+    }
+}
