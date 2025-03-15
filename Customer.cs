a2750e1207044d90b6bad7b5a1195a5518c041c4
Hanna Baranowska <s193531@student.pg.edu.pl>
2025-03-15 13:24:26 +0100

    optimalization and new functionalities

---
+++ Customer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Customer
+{
+    public int CustomerId { get; private set; }
+    public string KlientType { get; private set; }
+    public string Address { get; private set; }
+    private static int nextCustomer = 0;
+    public List<Order> customerOrders = new List<Order>();
+    public static List<Customer> allCustomers = new List<Customer>();
+    public Customer(string klientType, string address){
+        this.CustomerId = nextCustomer++;
+        this.KlientType = klientType;
+        this.Address = address;
+        allCustomers.Add(this);
+    }
+
+    public static void SeeCustomerOrders(int id)
+    {
+        Customer foundCustomer = allCustomers.Find(c => c.CustomerId == id);
+        if (foundCustomer == null)
+        {
+            Console.WriteLine($"Nie znaleziona klienta o ID: {id}");
+            return;
+        }
+        if(foundCustomer.customerOrders.Count == 0)
+        {
+            Console.WriteLine("Brak zamówień.");
+        }
+        else
+        {
+            if (foundCustomer.KlientType == "F")
+            {
+                foundCustomer.KlientType = "firma";
+            }
+            else
+            {
+                foundCustomer.KlientType = "osoba fizyczna";
+            }
+            Console.WriteLine($"Zamówienie dla klienta nr: {id}, typ klienta: ({foundCustomer.KlientType}):");
+            foreach (Order order in foundCustomer.customerOrders)
+            {
+                Console.WriteLine(order.product.Name);
+            }
+        }
+    }
+    public void AddCustomerOrder(Order order)
+    {
+        customerOrders.Add(order);
+    }
+    public static void ShowCustomerData(int id)
+    {
+        Customer foundCustomer = allCustomers.Find(c => c.CustomerId == id);
+        if(foundCustomer != null)
+        {
+            if (foundCustomer.KlientType == "F")
+            {
+                foundCustomer.KlientType = "firma";
+            }
+            else
+            {
+                foundCustomer.KlientType = "osoba fizyczna";
+            }
+            Console.WriteLine($"Id klienta: {id}, Klient: {foundCustomer.KlientType}," +
+            $" Adres: {foundCustomer.Address}");
+
+            SeeCustomerOrders(id);
+        }
+        else
+        {
+            Console.WriteLine($"Nie znaleziono klienta o ID {id}");
+        }
+    }
+}
+
---
+++ Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Product
+{
+    private int AmountOfMoney { get; set; }
+    public string Name { get; set; }
+
+    private static List<Product> boughtProducts = new List<Product>();
+    public Product(string name)
+    {
+        this.Name = name;
+    }
+    public static void AddProduct(Product product)
+    {
+        boughtProducts.Add(product);
+    }
+    public static void HowMuchProduct(string name)
+    {
+        int count = boughtProducts.Count(p => p.Name== name);
+        if(count == 1)
+        {
+            Console.WriteLine($"Produktu o nazwie: {name} kupiono {count} raz");
+        }
+        else
+        {
+            Console.WriteLine($"Produktu o nazwie: {name} kupiono {count} razy");
+        }
+    }
+}
+
